172c70d51583d6cfb8675b0ac8e7d7deea0927cc
Sean Duggan <sean.duggan@gmail.com>
2025-08-11 23:05:17 -0400

    Base Glider Changes

--- Assets/Fantacode Studios/Glide Controller/Scripts/Util/FSSystemsSetupGliding.cs
+++ Assets/Fantacode Studios/Glide Controller/Scripts/Util/FSSystemsSetupGliding.cs
@@ -1,15 +1,15 @@
 #if UNITY_EDITOR
 using UnityEditor;
 
-namespace FS_ThirdPerson
+namespace FS_Core
 {
     public partial class FSSystemsSetup
     {
         public static FSSystemInfo GlidingSystemSetup = new FSSystemInfo
         (
             characterType: CharacterType.Player,
-            enabled: false,
-            name: "Gliding System",
+            selected: false,
+            displayName: "Gliding System",
             prefabName: "Gliding Controller",
             welcomeEditorShowKey: "GlidingSystem_WelcomeWindow_Opened",
             mobileControllerPrefabName: ""
